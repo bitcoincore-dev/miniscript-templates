251062d7cd98e21fd992e87fca447f7ffd22018b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-11-07 18:00:22 -0500

    docker-start: if CI != True

--- docker.mk
+++ docker.mk
@@ -3,14 +3,11 @@ docker-start:## 	start docker on Linux or Darwin
 	@( \
 	    while ! docker system info > /dev/null 2>&1; do\
 	    echo 'Waiting for docker to start...';\
-	    if [[ '$(OS)' == 'Linux' ]]; then\
+	    if [[ '$(OS)' == 'Linux' ]] && [[ '$(CI)' != "True" ]]; then\
 	     type -P systemctl && systemctl restart docker.service || type -P service && service restart docker;\
 	    fi;\
-	    if [[ '$(OS)' == 'Darwin' ]]; then\
-	    echo $(CI);\
-	    if [[ '$(CI)' != 'True' ]]; then\
+	    if [[ '$(OS)' == 'Darwin' ]] && [[ '$(CI)' != "True" ]]; then\
 	     type -P docker && open --background -a /./Applications/Docker.app/Contents/MacOS/Docker || brew install --cask docker;\
-	    fi;\
 	    fi;\
 	sleep 1;\
 	done\
